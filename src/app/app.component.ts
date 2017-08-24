0af81fa12bac420de0cdc90553ddfaffbb47f8a2
tzmanics <tzmanics@gmail.com>
2017-08-23 22:53:28 -0400

    add some http calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  loadedCharacter: {};
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    const peopleAPI = 'https://swapi.co/api/people/1';
+    const planetAPI = 'https://swapi.co/api/planets/1';
+    let character = this.http.get(peopleAPI).map(res => res.json());
+    let characterHomeworld = this.http.get(planetAPI).map(res => res.json());
+  
+    Observable.forkJoin([ character, characterHomeworld ])
+      .subscribe(results => {
+        results[0].homeworld = results[1];
+        this.loadedCharacter = results[0];
+      })
+  }
 }
